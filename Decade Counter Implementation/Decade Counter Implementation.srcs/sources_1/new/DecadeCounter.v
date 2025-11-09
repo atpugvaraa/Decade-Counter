368796e128d7275cee8eba7d2b2b53e5107cfa79
atpugvaraa <atpugvaraa@gmail.com>
2025-11-09 13:33:08 +0000

    Setup Vivado Project

---
+++ Decade Counter Implementation/Decade Counter Implementation.srcs/sources_1/new/DecadeCounter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/09/2025 01:29:40 PM
+// Design Name: 
+// Module Name: DecadeCounter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DecadeCounter(
+
+    );
+endmodule
